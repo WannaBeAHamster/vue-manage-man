98e1c148d8b3132e83afd49e7046f81d47342c24
kfx <444845449@qq.com>
2023-04-21 10:43:44 +0800

    element-ui组件的全部引入和按需引入

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,16 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import ElementUI from 'element-ui'
+import {
+  Row,
+  Button
+} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
+// Vue.use(ElementUI)
+Vue.use(Row)
+Vue.use(Button)
 
 new Vue({
   router,
