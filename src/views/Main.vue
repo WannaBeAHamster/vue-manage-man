bae94dbd9d3fc6a5f74df45c812ed85e687cc0dd
kfx <444845449@qq.com>
2023-04-21 21:21:53 +0800

    头部区域编写完成

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,7 +5,9 @@
         <CommonAside></CommonAside>
       </el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header>
+          <CommonHeader></CommonHeader>
+        </el-header>
         <el-main>
           <!-- 路由匹配到的组件将渲染在这里 -->
           <router-view></router-view>
@@ -17,11 +19,17 @@
 
 <script>
 import CommonAside from '@/components/CommonAside.vue'
+import CommonHeader from '@/components/CommonHeader.vue'
 export default {
   components: {
-    CommonAside
+    CommonAside,
+    CommonHeader
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.el-header {
+  padding: 0;
+}
+</style>
