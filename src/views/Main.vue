6581569532826dc9744cfccada0021f3301bd89d
kfx <444845449@qq.com>
2023-04-21 11:26:08 +0800

    提交路由

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>主路由</h1>
+    <nav>
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/user">User</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
